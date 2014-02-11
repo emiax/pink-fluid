8591c73d0231ccba788939c022432858c3e11d82
Karl Johan Krantz <schwomp@gmail.com>
2014-02-11 14:55:09 +0100

    Cleaned up code managing textures with Texture2D class

--- src/pink-fluid.cpp
+++ src/pink-fluid.cpp
@@ -25,7 +25,7 @@
 
 #include <common/Init.h>
 #include <common/Shader.h>
-
+#include <common/Texture2D.h>
 #include <OrdinalGrid.h>
 #include <state.h>
 #include <simulator.h>
@@ -37,7 +37,7 @@ int main( void ) {
 
   //Initialize glfw
   init.glfw(4,1);
-
+  glfwSwapInterval(1);
   //Open a window
   GLFWwindow* window = init.window();
 
@@ -74,7 +74,9 @@ int main( void ) {
     0.0f, 0.0f,
     1.0f, 0.0f,
     1.0f, 1.0f,
-    0.0f, 1.0f
+    1.0f, 1.0f,
+    0.0f, 1.0f,
+    0.0f, 0.0f
   };
 
   GLuint vertexbuffer;
@@ -88,8 +90,7 @@ int main( void ) {
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
 
-  int w = 250, h = 250;
-  int d = 4;
+  int w = 100, h = 100;
   State prevState(w, h);
   OrdinalGrid<float>** velocities = new OrdinalGrid<float>*[2];
   std::cout << "Setting up basic state" << std::endl;
@@ -97,8 +98,9 @@ int main( void ) {
   velocities[1] = new OrdinalGrid<float>(w, h+1);
   for(int i = 0; i <= w; i++){
     for(int j = 0; j < h; j++){
-      velocities[0]->set(i,j,0.5f);
+      velocities[0]->set(i,j,i*0.1);
     }
+    
   }
   for(int i = 0; i < w; i++){
     for(int j = 0; j <= h; j++){
@@ -107,35 +109,16 @@ int main( void ) {
   }
   prevState.setVelocityGrid(velocities);
   State newState(w, h);
-  State tempState(w,h);
-  Simulator sim(w,h);
-
-  GLuint textureID;
-  glGenTextures(1, &textureID);
+  State tempState(w, h);
+  Simulator sim(w, h);
   
-  glBindTexture(GL_TEXTURE_2D, textureID);
-  glPixelStorei(GL_UNPACK_ALIGNMENT,1);	
-  std::cout << "Generated texture" << std::endl;
-  std::cout << "Created texture" << std::endl;
-  GLfloat *data = new GLfloat[h*w*d];
-  for (int i = 0; i < h; ++i) {
-    for (int j = 0; j < w; ++j){
-      data[i*w*d + j*d + 0] = 0.0f;
-      data[i*w*d + j*d + 1] = 0.0f;
-      data[i*w*d + j*d + 2] = 0.0f;
-      data[i*w*d + j*d + 3] = 1.0f;
-    }
-  }
-  std::cout << "Wrote to texture data" << std::endl;
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, w, h, 0, GL_RGBA, GL_FLOAT, data);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  std::cout << "Everything ready for first render pass" << std::endl;
+  Texture2D tex2D(w, h);
+
   float lastRun = glfwGetTime();
   do{
-    float deltaT = glfwGetTime()-lastRun;
     lastRun = glfwGetTime();
-    sim.step(&prevState, &newState,deltaT);
+    float deltaT = glfwGetTime()-lastRun;
+    sim.advect(&prevState, &newState, 1);
      
     
     int width, height;
@@ -147,21 +130,19 @@ int main( void ) {
 
     GLuint location = glGetUniformLocation(prog, "time");
     glUniform1f(location, glfwGetTime());
-    GLuint textureLocation = glGetUniformLocation(prog, "myFloatTex");
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    // Set our "myTextureSampler" sampler to user Texture Unit 0
-    glUniform1i(textureLocation, 0);
+
     for(int i=0;i<w;++i) {
       for(int j = 0; j < h; ++j){
-        data[i*w*d + j*d + 0] = newState.getVelocityGrid()[0]->get(i,j);
-        data[i*w*d + j*d + 1] = newState.getVelocityGrid()[1]->get(i,j);
-        data[i*w*d + j*d + 2] = 0.0f;
-        data[i*w*d + j*d + 3] = 1.0f;
+        tex2D.set(i,j,0, newState.getVelocityGrid()[0]->get(i,j));
+        tex2D.set(i,j,1, newState.getVelocityGrid()[1]->get(i,j));
+        tex2D.set(i,j,2, 0.0f);
+        tex2D.set(i,j,3, 1.0f);
       }
     }
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, w, h, 0, GL_RGBA, GL_FLOAT, data);
 
+    GLuint textureLocation = glGetUniformLocation(prog, "myFloatTex");
+    tex2D(GL_TEXTURE0);
+    glUniform1i(textureLocation, 0);
 
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
@@ -201,13 +182,12 @@ int main( void ) {
     newState = tempState;
   } // Check if the ESC key was pressed or the window was closed
   while( !glfwWindowShouldClose(window) );
-
+  std::cout << "Cleaning up!" << std::endl;
   // Close OpenGL window and terminate GLFW
   glfwDestroyWindow(window);
   glfwTerminate();
   glDeleteBuffers(1, &vertexbuffer);
   glDeleteBuffers(1, &uvbuffer);
   glDeleteVertexArrays(1, &VertexArrayID);
-  glDeleteTextures(1, &textureID);
   exit(EXIT_SUCCESS);
 }
