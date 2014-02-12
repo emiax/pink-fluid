f5b3b2c22fd14e78a4da16ce8b6fbad297426247
Karl Johan Krantz <schwomp@gmail.com>
2014-02-12 15:02:42 +0100

    Switched from array to struct for Velocity Grid

--- test/simulatorTest.cpp
+++ test/simulatorTest.cpp
@@ -10,29 +10,23 @@ protected:
     readState = new State(w, h);
     writeState = new State(w, h);
     sim = new Simulator(readState, writeState);
-
-    OrdinalGrid<float>** velocities = new OrdinalGrid<float>*[2];
-    velocities[0] = new OrdinalGrid<float>(w+1, h);
-    velocities[1] = new OrdinalGrid<float>(w, h+1);
+    VelocityGrid* velocities = new VelocityGrid(w,h);
 
     for(int i = 0; i <= w; i++){
       for(int j = 0; j < h; j++){
-        velocities[0]->set(i,j,0);
+        velocities->u->set(i,j,0);
       }
-      velocities[0]->set(i,0,1);
+      velocities->u->set(i,0,1);
     }
     for(int i = 0; i < w; i++){
       for(int j = 0; j <= h; j++){
-        velocities[1]->set(i,j,0);
+        velocities->v->set(i,j,0);
       }
     }
-    velocities[0]->set(1,1,1);
+    velocities->u->set(1,1,1);
     readState->setVelocityGrid(velocities);
     writeState->setVelocityGrid(velocities);
-    delete velocities[0];
-    delete velocities[1];
-    delete[] velocities;
-
+    delete velocities;
   }
 
   ~SimulatorTest() {
@@ -55,7 +49,7 @@ TEST_F(SimulatorTest, BackTrack){
 
 TEST_F(SimulatorTest, Advect){
   sim->advect(readState, writeState, 1.0f);
-  float v1 = writeState->getVelocityGrid()[0]->get(1,1);
+  float v1 = writeState->getVelocityGrid()->u->get(1,1);
   ASSERT_EQ(v1, 0.5);
 }
 
