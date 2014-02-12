5c648e54abd30a99502eaad45ea83ad618ec147c
J-Zeitler <jonze168@student.liu.se>
2014-02-12 09:57:37 +0100

    commented methods in Simulator

--- include/simulator.h
+++ include/simulator.h
@@ -15,10 +15,14 @@ public:
   void step(State * const readFrom, State* writeTo, float dt);
   void step(float dt);
 
+  // advection
   glm::vec2 backTrack(State const * readFrom, int i,int j, float dt); 
   void advect(State const * readFrom, State * writeTo, float dt);
 
+  // ext. forces
   void calcGravity(const OrdinalGrid<float> *fromVelocityGrid, OrdinalGrid<float> *toVelocityGrid, glm::vec2 g);
+
+  // pressure
   void calcDivergence(const OrdinalGrid<float> *fromVelocityGrid, OrdinalGrid<float> *toVelocityGrid);
   void jacobiIteration();
   void gradientSubtraction(const OrdinalGrid<float> *fromVelocityGrid, OrdinalGrid<float> *toVelocityGrid);
