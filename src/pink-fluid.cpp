0ed0476f9b7396423fe10444a890053790db206d
Karl Johan Krantz <schwomp@gmail.com>
2014-02-12 08:28:25 +0100

    Documentation!
    Commented the functionality implemented the previous commit.

--- src/pink-fluid.cpp
+++ src/pink-fluid.cpp
@@ -78,7 +78,7 @@ int main( void ) {
     0.0f, 1.0f,
     0.0f, 0.0f
   };
-
+  //Create us some buffers
   GLuint vertexbuffer;
   glGenBuffers(1, &vertexbuffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -89,18 +89,19 @@ int main( void ) {
   glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
-
+  //Set up the initial state.
   int w = 100, h = 100;
   State prevState(w, h);
   OrdinalGrid<float>** velocities = new OrdinalGrid<float>*[2];
-  std::cout << "Setting up basic state" << std::endl;
   velocities[0] = new OrdinalGrid<float>(w+1, h);
   velocities[1] = new OrdinalGrid<float>(w, h+1);
+  //X velocities
   for(int i = w/4; i <= 3*w/4; i++){
     for(int j = h/4; j < 3*h/4; j++){
       velocities[0]->set(i,j,1);
     }
   }
+  //Y velocities
   for(int i = w/4; i < 3*w/4; i++){
     for(int j = h/4; j <= 3*h/4; j++){
       velocities[1]->set(i,j,1);
@@ -110,7 +111,8 @@ int main( void ) {
   State newState(w, h);
   State tempState(w, h);
   Simulator sim(w, h);
-  
+
+  //Object which encapsulates a texture + The destruction of a texture.
   Texture2D tex2D(w, h);
 
   // float lastRun = glfwGetTime();
@@ -118,6 +120,7 @@ int main( void ) {
   do{
     // lastRun = glfwGetTime();
     // float deltaT = glfwGetTime()-lastRun;
+    // 1 is a constant time step here, so that we can test the functionality of advect. This should be changed to formula 3.2 specified on page 32.
     sim.advect(&prevState, &newState, 1);
      
     
@@ -131,22 +134,27 @@ int main( void ) {
     GLuint location = glGetUniformLocation(prog, "time");
     glUniform1f(location, glfwGetTime());
 
-
+    //Set the x,y positions in the texture, in order to visualize the velocity field.
+    //Currently directly plots the mac-grid, should use interpolation in order to use the corresponding cell-value, instead of the edge velocities.
     for(int j = 0; j < h; ++j){
         for(int i=0;i<w;++i) {
-        tex2D.set(i,j,0, newState.getVelocityGrid()[0]->get(i,j));
-        tex2D.set(i,j,1, newState.getVelocityGrid()[1]->get(i,j));
-        tex2D.set(i,j,2, 0.0f);
-        tex2D.set(i,j,3, 1.0f);
+        tex2D.set(i, j, 0, newState.getVelocityGrid()[0]->get(i,j));
+        tex2D.set(i, j, 1, newState.getVelocityGrid()[1]->get(i,j));
+        tex2D.set(i, j, 2, 0.0f);
+        tex2D.set(i, j, 3, 1.0f);
       }
     }
 
+    //Get the uniformlocation of the texture from the shader.
     GLuint textureLocation = glGetUniformLocation(prog, "myFloatTex");
+    //Use GL_TEXTURE0 as a textureposition for the texture.
     tex2D(GL_TEXTURE0);
+    //Set the uniform so that the uniform maps 0 -> GL_TEXTURE0
     glUniform1i(textureLocation, 0);
 
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
+    //Triangle coordinates
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(
                           0,                  // Location 0
@@ -157,7 +165,7 @@ int main( void ) {
                           (void*)0            // array buffer offset
                           );
     
-
+    //UV Texture coordinates
     glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
     glVertexAttribPointer(
                           1,                  //Location 1
