b91206ea0651febc6f13661feb75a1428d1e2219
Karl Johan Krantz <schwomp@gmail.com>
2014-03-07 11:01:02 +0100

    Compile error due to last commit

--- include/velocityGrid.h
+++ include/velocityGrid.h
@@ -3,11 +3,9 @@
 #include <ordinalGrid.h>
 #include <interfaces/quantity.h>
 #include <util.h>
-struct VelocityGrid : public Quantity {
+struct VelocityGrid{
   VelocityGrid(unsigned int w, unsigned int h);
   ~VelocityGrid();
-
-  virtual void advect(VelocityGrid const* const velocityGrid, const float dt);
 
   glm::vec2 getCell(unsigned int i, unsigned int j) const;
   glm::vec2 getLerp(glm::vec2 p) const;
